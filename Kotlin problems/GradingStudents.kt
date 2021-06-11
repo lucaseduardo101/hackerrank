773abbf51449da8c3039492d6ba44d85d572ca62
Lucas Eduardo Oliveira <lucaseduardo101@gmail.com>
2021-06-11 14:15:15 -0300

    Create GradingStudents.kt

---
+++ Kotlin problems/GradingStudents.kt
@@ -0,0 +1,59 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+fun gradingStudents(grades: Array<Int>): Array<Int> {
+    
+    var finalGrades: MutableList<Int> = mutableListOf()
+    
+    grades.forEach { 
+        if(it < 38){
+            finalGrades.add(it)
+        }else{
+            val rem = it.rem(5)
+            if(rem <= 2){
+                finalGrades.add(it)
+            }else{
+                finalGrades.add(it + (5-rem))
+            }    
+        }                      
+     }
+    
+    return finalGrades.toTypedArray()
+}
+
+fun main(args: Array<String>) {
+    val gradesCount = readLine()!!.trim().toInt()
+
+    val grades = Array<Int>(gradesCount, { 0 })
+    for (i in 0 until gradesCount) {
+        val gradesItem = readLine()!!.trim().toInt()
+        grades[i] = gradesItem
+    }
+
+    val result = gradingStudents(grades)
+
+    println(result.joinToString("\n"))
+}
